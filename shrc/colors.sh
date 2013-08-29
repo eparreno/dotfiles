0c2c7d9759df2681013fa35470df149bc0e6b438
Emili Parreno <emili@eparreno.com>
2013-08-29 13:54:36 +0200

    fix colors in zsh

--- shrc/colors.sh
+++ shrc/colors.sh
@@ -1,2 +1,7 @@
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
+
+if [[ -n $ZSH_VERSION ]]; then
+  # Find the option for using colors in ls, depending on the version: Linux or BSD
+  ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+fi
